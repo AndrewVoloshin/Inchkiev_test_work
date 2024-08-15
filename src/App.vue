a80fce05d28b955b0ed1900d3221caa07cd4702b
AndrewVoloshin <voloshinandy@gmai.com>
2024-08-15 18:55:26 +0300

    feat: add normalize css and setup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <FeedbackForm/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FeedbackForm from './components/FeedbackForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FeedbackForm
   }
 }
 </script>
---
+++ src/components/FeedbackForm.vue
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<template>
+    <div class="feedback_form">
+        <h1>herer</h1>
+    </div>
+</template>
