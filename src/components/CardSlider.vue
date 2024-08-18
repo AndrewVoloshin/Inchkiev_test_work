6ba610ffd52ca0bd5d90385dd0c64a6e48298780
AndrewVoloshin <voloshinandy@gmai.com>
2024-08-18 15:21:15 +0300

    fix: css big slider

--- src/components/CardSlider.vue
+++ src/components/CardSlider.vue
@@ -28,7 +28,7 @@ export default {
             <p class="card__subtitle3">IPSUM!</p>
             <p class="card__subtitle-Ipsum">IPSUM !</p>
             <p class="quote">LOREM IPSUM DOLOR SIT AMET,</p>
-            <p class="quote">CONSECTETUR ADIPISICING ELIT</p>
+            <p class="quote">CONSECTETUR ADIPISCING ELIT</p>
         </div>
 
         <div class="star">
@@ -76,6 +76,8 @@ export default {
     border: 1px solid black;
     overflow: hidden;
     color: #FFFFFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);;
+        
 }
 
 .eclipse {
@@ -100,10 +102,9 @@ export default {
     margin: 0;
     z-index: 1;
     font-size: 18px;
-    font-weight: 400;
+    font-weight: 500;
     text-shadow:
-        2px 2px 4px rgba(0, 0, 0, 0.5);
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        4px 4px 4px rgba(0, 0, 0, 0.25);
 }
 
 .card__subtitle-Ipsum {
@@ -150,6 +151,7 @@ export default {
 }
 
 .quote {
+    font-weight: 500;
     margin: 0;
     margin-left: 284px;
     font-size: 15px;
@@ -174,14 +176,11 @@ export default {
 }
 
 .eclipses-img {
-    /* width: 642.98px; */
     width: 564.98px;
-
     height: 646.58px;
     position: absolute;
     top: 285.55px;
     right: 84.02px;
-    transform: rotate(359deg);
 }
 
 .eclipses-img img {
@@ -289,15 +288,13 @@ export default {
 }
 
 
-@media screen and (max-width: 768px) {
-
-}
+@media screen and (max-width: 768px) {}
 
 @media screen and (min-width: 770px) {
     .eclipses-css {
         display: none;
     }
-   
+
 }
 
 
@@ -315,26 +312,94 @@ export default {
 
 @media screen and (min-width: 1440px) {
 
-
-
-
-
-
     .card {
         position: relative;
         width: calc(100% - 83px);
     }
 
 
+    .card {
+        width: 1370px;
+        height: 708px;
+        border-radius: 0 90px 90px 0;
+    }
+
+    .eclipse {
+        left: 84.2px;
+        top: -171.17px;
+    }
+
+    .eclipse__paint {
+        width: 463.85px;
+        height: 240.79px;
+    }
+
+    .eclipse__text {
+        left: 113.19px;
+    }
+
+    .card__content {
+        padding: 116px 252px 205px 160px;
+    }
+
+    .card__title {
+        font-size: 60px;
+        margin-bottom: 3px;
+    }
+
+    .card__subtitle1 {
+        font-size: 60px;
+        text-shadow:
+            2px 2px 4px rgba(0, 0, 0, 0.5),
+
+            -1px -1px 0 rgba(0, 0, 0, 0.8),
+            1px -1px 0 rgba(0, 0, 0, 0.8),
+            -1px 1px 0 rgba(0, 0, 0, 0.8),
+            1px 1px 2px rgba(0, 0, 0, 0.5);
+        margin-bottom: 11px;
+        line-height: 70px;
+
+    }
+
+    .card__subtitle2 {
+        font-size: 60px;
+        margin-left: 289px;
+        margin-bottom: 12px;
+    }
+
+    .card__subtitle3 {
+        display: none;
+    }
+
+    .card__subtitle-Ipsum {
+        display: block;
+        font-size: 60px;
+        margin: 0;
+        margin-left: 141px;
+        margin-bottom: 41px;
+    }
+
+    .star {
+        width: 160px;
+        height: 160px;
+        top: 357px;
+        left: 0px;
+    }
+
+    .eclipses-img {
+        width: 642.98px;
+        top: 48.55px;
+    }
 
 
-   
+    .slider__button {
+        top: 294px;
+        right: 73.3px;
+    }
 
     .eclipse-pagecount {
         display: none;
     }
-
-
 
 }
 
@@ -353,152 +418,4 @@ export default {
         text-align: center;
     }
 }
-
-
-
-/* .card {
-    position: relative;
-    width: 1370px;
-    height: 708px;
-    background-color: #027D84;
-    border-radius: 0 90px 90px 0;
-    border: 1px solid black;
-    overflow: hidden;
-    color: #FFFFFF;
-}
-
-.eclipse {
-    position: absolute;
-    left: 84.2px;
-    top: -171.17px;
-}
-
-.eclipse__paint {
-    width: 463.85px;
-    height: 240.79px;
-    background-color: #AFB3B4;
-    border-radius: 50%;
-    transform: rotate(13.28deg);
-}
-
-.eclipse__text {
-    position: absolute;
-    bottom: 30.43px;
-    left: 113.19px;
-    color: #FFFFFF;
-    margin: 0;
-    z-index: 1;
-    font-size: 18px;
-    font-weight: 400;
-    text-shadow:
-        2px 2px 4px rgba(0, 0, 0, 0.5);
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-}
-
-
-.card__content {
-    padding: 116px 252px 205px 160px;
-}
-
-.card__title {
-    font-weight: bold;
-    font-size: 60px;
-    line-height: 64px;
-    margin: 0;
-    margin-bottom: 5px;
-}
-
-.card__subtitle1 {
-    font-weight: 400;
-    font-size: 60px;
-    text-shadow:
-        2px 2px 4px rgba(0, 0, 0, 0.5),
-       
-        -1px -1px 0 rgba(0, 0, 0, 0.8),
-        1px -1px 0 rgba(0, 0, 0, 0.8),
-        -1px 1px 0 rgba(0, 0, 0, 0.8),
-        1px 1px 2px rgba(0, 0, 0, 0.5);
-    margin: 0;
-    margin-left: 43px;
-    margin-bottom: 2px;
-    line-height: 70px;
-
-}
-
-.card__subtitle2 {
-    font-weight: 400;
-    font-size: 60px;
-    margin: 0;
-    margin-left: 289px;
-    margin-bottom: 3px;
-
-
-}
-
-.card__subtitle3 {
-    font-weight: 400;
-    font-size: 60px;
-    margin: 0;
-    margin-left: 141px;
-    margin-bottom: 41px;
-}
-
-.quote {
-    margin: 0;
-    margin-left: 284px;
-    font-size: 15px;
-    line-height: 150%;
-}
-
-.star {
-    width: 160px;
-    height: 160px;
-    position: absolute;
-    top: 357px;
-    left: 0px;
-}
-
-.star img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    width: 100%;
-}
-
-.eclipses-img {
-    width: 642.98px;
-    height: 646.58px;
-    position: absolute;
-    top: 49.55px;
-    right: 84.02px;
-}
-
-.eclipses-img img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    width: 100%;
-}
-
-.slider__button {
-    width: 120px;
-    height: 120px;
-    position: absolute;
-    top: 294px;
-    right: 74.3px;
-    cursor: pointer;
-}
-
-.slider__button img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    width: 100%;
-} */
 </style>
